c70291279caaf0b7947c362665a833a208d444e2
Divyanshu Pairhar <divyanshu1447@gmail.com>
2025-11-21 19:11:54 +0530

    engine: functions

---
+++ engine/functions/helloWorld.go
@@ -0,0 +1,28 @@
+package functions
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/inngest/inngestgo"
+	"github.com/inngest/inngestgo/step"
+)
+
+func HelloWorld(
+	ctx context.Context,
+	input inngestgo.Input[AccountCreatedEvent],
+) (any, error) {
+	result, err := step.Run(ctx, "hell-world", func(ctx context.Context) (bool, error) {
+		slog.InfoContext(ctx, "hello world")
+		return true, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	slog.InfoContext(ctx, "step result", slog.Bool("result", result))
+	return nil, nil
+}
+
+type AccountCreatedEvent inngestgo.GenericEvent[AccountCreatedEventData]
+type AccountCreatedEventData struct {
+}
--- engine/main.go
+++ engine/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "log/slog"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"github.com/divyanshu-parihar/chainFlow/functions"
+	"github.com/inngest/inngestgo"
+)
 
 func main() {
-	slog.Info("Engine Starting..")
+	ctx := context.Background()
+	client, err := inngestgo.NewClient(inngestgo.ClientOpts{
+		AppID: "core",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = inngestgo.CreateFunction(
+		client,
+		inngestgo.FunctionOpts{
+			ID:   "hello-world",
+			Name: "Hello World",
+		},
+		inngestgo.EventTrigger("api/hello.world", nil),
+		functions.HelloWorld,
+	)
+	if err != nil {
+		panic(err)
+	}
+	slog.InfoContext(ctx, "Engine Started...")
+	http.ListenAndServe(":8080", client.Serve())
 }
